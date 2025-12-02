aeb6c01af1a94ba0e8ad41d450df7eb8452d5e32
Sam Duemler <samduemler@gmail.com>
2025-12-01 19:54:49 -0500

    Day 1 Sol 2

--- day1/day1.py
+++ day1/day1.py
@@ -1,7 +1,10 @@
+import math
+
 input = open("day1/input.txt", "r")
 lines = input.readlines()
 start = 50
 counter = 0
+passCounter = 0
 
 for line in lines:
     line = line.rstrip()
@@ -9,15 +12,25 @@ for line in lines:
     dist = int(line[1:])
     if dir == "L":
         if start - dist < 0:
-            start = ((start - dist) % 100)
-        else:
-            start -= dist
+            if start == 0:
+                passCounter -= 1
+            while start - dist < 0:
+                passCounter += 1
+                dist -= 100
+        start -= dist
     elif dir == "R":
         if start + dist > 99:
-            start = ((start + dist) % 100)
-        else:
-            start += dist
+            while start + dist > 99:
+                passCounter += 1
+                dist -= 100
+        start += dist
+        if start == 0:
+            passCounter -= 1
     if start == 0:
         counter += 1
+    print("start " + str(start))
+    print("passCounter " + str(passCounter))
 
 print(counter)
+
+print(counter + passCounter)
