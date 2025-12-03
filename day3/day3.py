7efbdab78ed54cc23b4465d79603560c424c08ca
Sam Duemler <samduemler@gmail.com>
2025-12-03 16:39:34 -0500

    Day 3 Sol 2

--- day3/day3.py
+++ day3/day3.py
@@ -1,6 +1,7 @@
 input = open("day3/input.txt", "r")
 lines = input.readlines()
 totalJolts = 0
+totalBigJolts = 0
 
 for line in lines:
     line = line.rstrip()
@@ -18,5 +19,33 @@ for line in lines:
                 secondBattery = int(line[x+1])
     totalJolts += int(str(firstBattery) + str(secondBattery))
 
+    batteries = line
+    checkBatteries = batteries
+    selectedBatteries = [0]*12
+    start = 0
+    for x in range(12):
+        end = len(batteries) - 11 + x
+        swapped = False
+        selectedBatteries[x] = int(batteries[start])
+        start += 1
+        if start < end:
+            checkBatteries = batteries[start:end]
+            checks = 0
+            initial = start
+            for y in checkBatteries:
+                checks += 1
+                if int(y) > selectedBatteries[x]:
+                    start = initial
+                    selectedBatteries[x] = int(y)
+                    start += checks
+                                
+    selectedBatteries = ''.join(list(map(str, selectedBatteries)))
+    totalBigJolts += int(selectedBatteries)       
+        
+
+
+
 print(totalJolts)
+
+print(totalBigJolts)
     
