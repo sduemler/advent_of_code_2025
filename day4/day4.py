25e6413bbc07b34acab9e6aabb4d4729d929f4f4
Sam Duemler <samduemler@gmail.com>
2025-12-04 11:34:54 -0500

    Day 4 Sol 2

--- day4/day4.py
+++ day4/day4.py
@@ -47,3 +47,47 @@ for x in range(len(rolls)):
                 validRolls += 1
 
 print(validRolls)
+
+finalRolls = -1
+validRolls = 0
+
+while (finalRolls < validRolls):
+    finalRolls = validRolls
+    for x in range(len(rolls)):
+        roll = rolls[x]
+        for y in range(len(roll)):
+            if roll[y] == '@':
+                adj = 0
+                if x > 0:
+                    #check the above row
+                    if y > 0:
+                        if rolls[x-1][y-1] == '@':
+                            adj += 1
+                    if rolls[x-1][y] == '@':
+                        adj += 1
+                    if y < len(roll)-1:
+                        if rolls[x-1][y+1] == '@':
+                            adj += 1
+                if x < len(rolls)-1:
+                    #check the below row
+                    if y > 0:
+                        if rolls[x+1][y-1] == '@':
+                            adj += 1
+                    if rolls[x+1][y] == '@':
+                        adj += 1
+                    if y < len(roll)-1:
+                        if rolls[x+1][y+1] == '@':
+                            adj += 1
+                if y > 0:
+                    #check left
+                    if roll[y-1] == '@':
+                        adj += 1
+                if y < len(roll)-1:
+                    #check right
+                    if roll[y+1] == '@':
+                        adj += 1
+                if adj < 4:
+                    rolls[x][y] = 'x'
+                    validRolls += 1
+
+print(finalRolls)
