880b611da0db2d232ac1f955159efc39839b346a
Sam Duemler <samduemler@gmail.com>
2025-12-06 19:30:21 -0500

    Day 6 Sol 2

--- day6/day6.py
+++ day6/day6.py
@@ -2,6 +2,7 @@ input = open("day6/input.txt", "r")
 lines = input.readlines()
 homework = []
 total = 0
+grandTotal = 0
 
 for line in lines:
     line = line.rstrip().split(' ')
@@ -21,4 +22,50 @@ for x in range(len(homework[0])):
             added += int(homework[y][x])
             multed *= int(homework[y][x])
 
-print(total)
+# print(total)
+
+
+inputs = []
+for line in lines:
+    letters = []
+    for letter in line:
+        if letter == '\n':
+            continue
+        else:
+            letters.append(letter)
+    inputs.append(letters)
+
+while(" " in inputs[len(inputs)-1]):
+    for x in inputs[len(inputs)-1]:
+        if x == ' ':
+            inputs[len(inputs)-1].remove(x)
+
+modifiers = inputs[len(inputs)-1]
+inputs.pop()
+
+sepInputs = []
+for x in range(len(inputs[0])):
+    sep = []
+    for y in range(len(inputs)):
+        sep.append(inputs[y][x])
+    sepInputs.append(''.join(sep))
+
+modd = 0
+inputs = []
+inputTotal = 0
+for x in sepInputs:
+    if str.isspace(x):
+        modd += 1
+        grandTotal += inputTotal
+        inputTotal = 0
+    else:
+        if modifiers[modd] == '+':
+            inputTotal += int(x.strip())
+        else:
+            if inputTotal == 0:
+                inputTotal += 1
+            inputTotal *= int(x.strip())
+grandTotal += inputTotal
+
+print(grandTotal)
+        
